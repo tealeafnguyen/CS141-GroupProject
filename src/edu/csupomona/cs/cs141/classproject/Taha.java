010fcb5978f8c08c521b57e80acb34a61698b954
ButtholeRipper <tealeafnguyen@gmail.com>
2014-11-16 13:41:41 -0800

    Update Taha.java
    Adding attributes to the player class

--- src/edu/csupomona/cs/cs141/classproject/Taha.java
+++ src/edu/csupomona/cs/cs141/classproject/Taha.java
@@ -7,15 +7,39 @@ package edu.csupomona.cs.cs141.classproject;
  * @author Isa
  *
  */
+ 
+ //Ultimately, we should move the move method into the game engine
+ //For now I'm adding in attributes that the player should have
+ 
 public class Taha extends Entity implements GridMember{
 	
 	private int playerDirection;
 	
-	public Taha(){
+	private int lives;
+	
+	private int ammo;
+	
+	public Taha(){ //Going to change this constructor where lives = 3 and ammo = 1
 		playerDirection = 1;
 	}
 	
-	public void move(String direction, GameEngine grid){
+	public int showLives(){
+		return lives;
+	}
+	
+	public int showAmmo(){
+		return ammo;
+	}
+	
+	public void shoot(){
+		ammo--;
+	}
+	
+	public void dies(){ //when a ninja kills the player, this method should be called, then another method
+		lives--;    // that places the player at spawn should be called
+	}
+	
+	public void move(String direction, GameEngine grid){ //going to move this to the GameEngine
 		int[] playerPosition = grid.getPlayerPostion();
 		int row = playerPosition[0];
 		int col = playerPosition[1];
