f3bca0cc6fd8b997d6f4c096614bcc15ba5a6f9d
ButtholeRipper <tealeafnguyen@gmail.com>
2014-11-20 12:12:29 -0800

    Updated with my working version

--- src/edu/csupomona/cs/cs141/classproject/Taha.java
+++ src/edu/csupomona/cs/cs141/classproject/Taha.java
@@ -14,38 +14,71 @@ package edu.csupomona.cs.cs141.classproject;
 public class Taha extends Entity implements GridMember{
 	
 	private int lives;
-	
+
 	private int ammo;
+
+	private int cantDieDuration;
 	
-	public Taha(){ 
+	private int playerDirection;
+	
+	
+	public Taha() {
 		lives = 3;
 		ammo = 1;
+		playerDirection = 1;
 	}
-	
-	public int showLives(){
+
+	public int showLives() {
 		return lives;
 	}
-	
-	public int showAmmo(){
+
+	public int showAmmo() {
 		return ammo;
 	}
 	
-	public void shoot(){
+	public int showCantDieTime(){
+		return cantDieDuration;
+	}
+	
+	public void cantDieCheck(){
+		cantDieDuration--;
+	}
+
+	public void shoot() {
 		ammo--;
 	}
-	
-	public void dies(){ //when a ninja kills the player, this method should be called, then another method
-		lives--;    // that places the player at spawn should be called
+
+	public void dies() { 						
+		lives--; 
 	}
 	
-	public String toString(){
+	public void addAmmo(){
+		ammo++;
+	}
+	
+	public void isWalkingOnSunshine(){
+		cantDieDuration = 5;
+	}
+
+
+	public String toString() {
 		String result = "[P]";
 		return result;
 	}
+
 	
-	public boolean isSeen(){
+	public boolean isSeen() {
 		return true;
 	}
 	
+	public void setPlayerDirection(int direction) {
+		playerDirection = direction;
+	}
 	
+	public int getPlayerDirection() {
+		return playerDirection;
+	}
+
+
+}
 }
