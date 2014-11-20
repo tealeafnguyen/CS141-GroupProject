18d316340d58aa84c9266ffdde063b19eb024ea9
ButtholeRipper <tealeafnguyen@gmail.com>
2014-11-20 11:45:02 -0800

    Updated ninja with new fields
    Ninjas will now realize they have been shot and will stay dead to work with the shoot() method in grid.
    The earlier shoot() method would ruin their families and the ninjas wont be able to go home for the holidays, once they have been shot. The shoot() method would replace their place on the grid with an empty member. But that EmptyMember still had the ninja's location, so these assholes will still kill you even if it was an empty cell or the ninja's move() method would still "move" a ninja so its like you shot a ninja's double, but ended up looking retarded.
    
    These changes to the Ninja class will make it so the ninja will return a boolean that they have been shot. The grid now possesses a method that checks if the ninja is dead, if they are dead, they cant move or kill you or respawn on the grid at all.
    
    -added boolean isShot
    -added boolean method totallyGotShot()
    -added void method gotShot()

--- src/edu/csupomona/cs/cs141/classproject/Ninja.java
+++ src/edu/csupomona/cs/cs141/classproject/Ninja.java
@@ -7,97 +7,105 @@ import java.util.Random;
  * @author Isa
  *
  */
-public class Ninja extends Entity implements GridMember{
-	
+public class Ninja extends Entity implements GridMember {
+
 	private int direction;
-	
+
 	private boolean playerInSight;
 	
-	public int getDirection(){
+	private boolean isShot;
+
+	public int getDirection() {
 		return direction;
 	}
-	
-	public void move(Taha thePlayer){
-		
+
+	public void updatePosition(int row, int col) {
+
 	}
-	
-	public void updatePosition(int row, int col){
-		
-	}
-	
-	
-	//incomplete method that takes care of the "AI" of the ninja as well as moving randomly
-	public void setDirection(Taha player){
+
+	// incomplete method that takes care of the "AI" of the ninja as well as
+	// moving randomly
+	public void setDirection(Taha player) {
 		int[] playerCoordinates = player.getPosition();
 		int[] ninjaCoordinates = getPosition();
 		Random rand = new Random();
-		if(playerCoordinates[0] == ninjaCoordinates[0]){
-			if(playerCoordinates[1] > ninjaCoordinates[1]){
+		if (playerCoordinates[0] == ninjaCoordinates[0]) {
+			if (playerCoordinates[1] > ninjaCoordinates[1]) {
 				direction = 2;
 			} else {
 				direction = 4;
 			}
 			playerInSight = true;
-		} 
-		else if(playerCoordinates[1] == ninjaCoordinates[1]){
-				if(playerCoordinates[0] > ninjaCoordinates[0]){
-					direction = 1;
-				} else {
-					direction = 3;
-				}
-				playerInSight = true;
-		}
-		else {
+		} else if (playerCoordinates[1] == ninjaCoordinates[1]) {
+			if (playerCoordinates[0] > ninjaCoordinates[0]) {
+				direction = 1;
+			} else {
+				direction = 3;
+			}
+			playerInSight = true;
+		} else {
 			direction = rand.nextInt(4) + 1;
 		}
-			
+
 	}
-	
-	public void setDirection(int row, int col){
-		
+
+	public void setDirection(int row, int col) {
+
 		int[] ninjaCoordinates = getPosition();
 		Random rand = new Random();
-		if(row == ninjaCoordinates[0]){
-			
-			if(col > ninjaCoordinates[1]){
+		if (row == ninjaCoordinates[0]) {
+
+			if (col > ninjaCoordinates[1]) {
 				direction = 2;
 			} else {
 				direction = 4;
 			}
 			playerInSight = true;
-			 
-		} 
-		else if(col == ninjaCoordinates[1]){
-			
-				if(row > ninjaCoordinates[0]){
-					direction = 1;
-				} else {
-					direction = 3;
-				}
-				playerInSight = true;
-			 
-		}
-		else {
+
+		} else if (col == ninjaCoordinates[1]) {
+
+			if (row > ninjaCoordinates[0]) {
+				direction = 1;
+			} else {
+				direction = 3;
+			}
+			playerInSight = true;
+
+		} else {
 			direction = rand.nextInt(4) + 1;
 			playerInSight = false;
 		}
-			
+
 	}
-	
-	public boolean getPlayerInSight(){
+
+	public boolean getPlayerInSight() {
 		return playerInSight;
 	}
-	
-	public void resetPlayerInSight(){
+
+	public void resetPlayerInSight() {
 		playerInSight = false;
 	}
 	
-
-	public String toString(){
-		String result;
-		
-		result = "[N]";
-		return result;
+	public void gotShot(){
+		isShot = true;
 	}
 	
+	public boolean totallyGotShot(){
+		return isShot;
+	}
+
+	@Override
+	public String toString() {
+		String result;
+		
+		if(isShot){
+			result = "[#]";
+			return result;
+		}
+
+		else{
+			result = "[N]";
+			return result;
+		}
+	}
 }
