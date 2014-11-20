774cb3b27cbbef74c135a73cd0d8d73f131f50dc
ButtholeRipper <tealeafnguyen@gmail.com>
2014-11-20 12:11:03 -0800

    Updated with my working version

--- src/edu/csupomona/cs/cs141/classproject/Invincibility.java
+++ src/edu/csupomona/cs/cs141/classproject/Invincibility.java
@@ -1,5 +1,14 @@
 package edu.csupomona.cs.cs141.classproject;
 
 public class Invincibility extends PowerUp implements GridMember {
-
+	public String toString() {
+		String result;
+		if (isSeen()) {
+			result = "[I]";
+		} else
+			result = "[I]";
+		return result;
+	}
+	
+}
 }
