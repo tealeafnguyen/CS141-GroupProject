dbbce0f2ade9e9d64fc57abdcc52594ba82a9519
ButtholeRipper <tealeafnguyen@gmail.com>
2014-11-20 12:15:02 -0800

    Updated with my working version

--- src/edu/csupomona/cs/cs141/classproject/Radar.java
+++ src/edu/csupomona/cs/cs141/classproject/Radar.java
@@ -9,4 +9,13 @@ package edu.csupomona.cs.cs141.classproject;
  */
 public class Radar extends PowerUp implements GridMember {
 
+	public String toString() {
+		String result;
+		if (isSeen()) {
+			result = "[r]";
+		} else
+			result = "[r]";
+		return result;
+	}
+	
 }
