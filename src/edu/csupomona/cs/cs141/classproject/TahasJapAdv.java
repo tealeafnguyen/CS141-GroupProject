7fa1ecf9e79c139ea46a8f6885ca140c0efc2f14
TheJericho <TheJericho@users.noreply.github.com>
2014-12-02 20:51:18 -0800

    Added comments to TahaJapAdv

--- src/edu/csupomona/cs/cs141/classproject/TahasJapAdv.java
+++ src/edu/csupomona/cs/cs141/classproject/TahasJapAdv.java
@@ -1,4 +1,18 @@
 /**
+ * CS 141: Introduction to Programming and Problem Solving
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Programming Project
+ *
+ * TahasJapAdv class is only used to start the program,
+ * this class contains the main method.
+ *
+ * Team Crazy Bananas
+ * Taha Khan
+ * Farzad Kosar
+ * Yool Weeji Jeon (James)
+ * Isaac Gonzalez
+ * Thomas Nguyen
  * 
  */
 package edu.csupomona.cs.cs141.classproject;
@@ -7,14 +21,16 @@ import java.io.Serializable;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * @author Isa
- *
- */
-public class TahasJapAdv implements Serializable {
+
+public class TahasJapAdv{
 
 	/**
-	 * @param args
+	 * This is the main method used to either start the command
+	 * line version of the game or the graphics version of the game
+	 * depending on the argument given at launch.
+	 * @param args the array of arguments given at launch,
+	 * currently the only argument is "-gui" which launches
+	 * the gui. 
 	 */
 
 
@@ -32,17 +48,5 @@ public class TahasJapAdv implements Serializable {
 			}
 		}
 	}
-	// This here is just a placeHolder for functionality.
-	// Please rework the UI and Main class later
-	// public static void main(String[] args) {
-	// UserInterface ui = new UserInterface();
-	// ui.options();
-	// }
 
-	// testing gui
-	// public static void main(String[] args){
-	//
-	// GUIMainMenu gui = new GUIMainMenu();
-	//
-	// }
 }
