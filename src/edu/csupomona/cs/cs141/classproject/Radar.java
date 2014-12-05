709d53bef2333b1d10da30201ab5a32615d6cb42
TheJericho <TheJericho@users.noreply.github.com>
2014-12-04 23:14:06 -0800

    commented radar

--- src/edu/csupomona/cs/cs141/classproject/Radar.java
+++ src/edu/csupomona/cs/cs141/classproject/Radar.java
@@ -1,18 +1,27 @@
 /**
- * 
- */
-package edu.csupomona.cs.cs141.classproject;
-
-import java.io.Serializable;
-
-import javax.swing.ImageIcon;
-
-/**
- * @author Isa
- *
- */
+* CS 141: Introduction to Programming and Problem Solving
+* Professor: Edwin Rodr&iacute;guez
+*
+* Programming Project
+*
+* This class, Radar, is an extension of the class, PowerUp, because this
+* class needs to be able to read and take the methods to be used in this class
+* to function properly. It also implements Serializable to be able to save and
+* load.
+* 
+* Team Crazy Bananas
+* Taha Khan
+* Farzad Kosar
+* Yool Weeji Jeon (James)
+* Isaac Gonzalez
+* Thomas Nguyen
+*/ 
 public class Radar extends PowerUp implements GridMember, Serializable {
-
+	/**
+	 * This class takes in the field, result, and prints out if the result is
+	 * seen or not in the game field of the grid. Then, it prints out the result
+	 * according to the if and else statement.
+	 */
 	public String toString() {
 		String result;
 		if (isSeen()) {
@@ -21,9 +30,14 @@ public class Radar extends PowerUp implements GridMember, Serializable {
 			result = "[*]";
 		return result;
 	}
-	public ImageIcon toImage(){
+
+	/**
+	 * This method is exactly same as method as above, toString(). This method
+	 * was created for the use of GUI.
+	 */
+	public ImageIcon toImage() {
 		ImageIcon result;
-		if(isSeen()){
+		if (isSeen()) {
 			result = new ImageIcon("radarpowerup.jpg");
 			return result;
 		} else {
@@ -31,6 +45,5 @@ public class Radar extends PowerUp implements GridMember, Serializable {
 			return result;
 		}
 	}
-	
-	
+
 }
