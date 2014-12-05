217707a24b4976df3b1a0851ebc43de9380deb18
TheJericho <TheJericho@users.noreply.github.com>
2014-12-04 23:11:16 -0800

    Commented ExtraBullet -James

--- src/edu/csupomona/cs/cs141/classproject/ExtraBullet.java
+++ src/edu/csupomona/cs/cs141/classproject/ExtraBullet.java
@@ -1,17 +1,33 @@
 /**
- * 
- */
+* CS 141: Introduction to Programming and Problem Solving
+* Professor: Edwin Rodr&iacute;guez
+*
+* Programming Project
+*
+* This class, ExtraBullet, is an extension of the class, PowerUp, because this
+* class needs to be able to read and take the methods to be used in this class
+* to function properly. It also implements Serializable to be able to save and
+* load.
+*
+* Team Crazy Bananas
+* Taha Khan
+* Farzad Kosar
+* Yool Weeji Jeon (James)
+* Isaac Gonzalez
+* Thomas Nguyen
+*/ 
 package edu.csupomona.cs.cs141.classproject;
 
 import java.io.Serializable;
 
 import javax.swing.ImageIcon;
 
-/**
- * @author Isa
- *
- */
 public class ExtraBullet extends PowerUp implements Serializable {
+	/**
+	 * This class takes in the field, result, and prints out if the result is
+	 * seen or not in the game field of the grid. Then, it prints out the result
+	 * according to the if and else statement.
+	 */
 	public String toString() {
 		String result;
 		if (isSeen()) {
@@ -21,6 +37,10 @@ public class ExtraBullet extends PowerUp implements Serializable {
 		return result;
 	}
 
+	/**
+	 * This method is exactly same as method as above, toString(). This method
+	 * was created for the use of GUI.
+	 */
 	@Override
 	public ImageIcon toImage() {
 		// TODO Auto-generated method stub
