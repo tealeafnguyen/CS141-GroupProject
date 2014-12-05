dec04fe9947cef48b1d9ba781e5860b70e79cdc8
TheJericho <TheJericho@users.noreply.github.com>
2014-12-04 23:12:43 -0800

    commented Invincibility

--- src/edu/csupomona/cs/cs141/classproject/Invincibility.java
+++ src/edu/csupomona/cs/cs141/classproject/Invincibility.java
@@ -1,3 +1,21 @@
+/**
+* CS 141: Introduction to Programming and Problem Solving
+* Professor: Edwin Rodr&iacute;guez
+*
+* Programming Project
+*
+* This class, Invincibility, is an extension of the class, PowerUp, because this
+* class needs to be able to read and take the methods to be used in this class
+* to function properly. It also implements Serializable to be able to save and
+* load.
+* 
+* Team Crazy Bananas
+* Taha Khan
+* Farzad Kosar
+* Yool Weeji Jeon (James)
+* Isaac Gonzalez
+* Thomas Nguyen
+*/  
 package edu.csupomona.cs.cs141.classproject;
 
 import java.io.Serializable;
@@ -5,6 +23,11 @@ import java.io.Serializable;
 import javax.swing.ImageIcon;
 
 public class Invincibility extends PowerUp implements GridMember, Serializable {
+	/**
+	 * This class takes in the field, result, and prints out if the result is
+	 * seen or not in the game field of the grid. Then, it prints out the result
+	 * according to the if and else statement.
+	 */
 	public String toString() {
 		String result;
 		if (isSeen()) {
@@ -13,6 +36,10 @@ public class Invincibility extends PowerUp implements GridMember, Serializable {
 			result = "[*]";
 		return result;
 	}
+	/**
+	 * This method is exactly same as method as above, toString(). This method
+	 * was created for the use of GUI.
+	 */
 	public ImageIcon toImage(){
 		ImageIcon result;
 		if(isSeen()){
